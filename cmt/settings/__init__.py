4ed189add4283459148fa6b03504c70037b2299c
Rupam Dhara <thedevgenius83@gmail.com>
2026-06-02 07:19:29 +0530

    Uopdares

--- cmt/settings/__init__.py
+++ cmt/settings/__init__.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load .env file
 
 ENV = os.getenv("ENVIRONMENT", "dev")
 
--- cmt/settings/base.py
+++ cmt/settings/base.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = "y2x1x-4^97offa$ruwd$l+_vxnln=%1l7o77t#aq^ci$1=eny&"
 
 
 # Application definition
